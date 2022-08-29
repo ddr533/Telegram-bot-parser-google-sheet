52b0cb232d2a4045a6c03227e991bd22a37e1d18
ddr533 <ddr533@yandex.ru>
2022-08-29 16:04:27 +0700

    ADD new files bot.py, parser_google_sheet.py

---
+++ bot.py
@@ -0,0 +1,38 @@
+import telebot
+from telebot import types
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import pickle
+import shelve
+
+bot = telebot.TeleBot("5216151294:AAGqtj1iTiQpqIKIQHARZGpJmNANH6whv_k")
+places=[['ПАВЛОВСКИЙ ТРАКТ','0'],['ЛЕНТА','1'],['ЛЕНИНА','2'],['ОГНИ','3'],['Новый рынок','4']]
+with open('grafic_dump.dat', 'rb') as f:
+    total_graf = pickle.load(f)
+
+@bot.message_handler(content_types=["text"])
+def default_test(message):
+    keyboard = types.InlineKeyboardMarkup()
+    for i in places:
+        button = types.InlineKeyboardButton(text = i[0], callback_data = i[1])
+        keyboard.add(button)
+    bot.send_message(message.chat.id, "Выберете место из списка:", reply_markup = keyboard)
+
+@bot.callback_query_handler(func = lambda call: True)
+def ans(call):
+    message = call.message
+    print(int(call.data))
+    if int(call.data) in range(5):
+        keyboard = types.InlineKeyboardMarkup()
+        hair_graf = total_graf[int(call.data)]
+        for masters in hair_graf:
+            button = types.InlineKeyboardButton(text = masters[0], callback_data = str(masters[1]))
+            keyboard.add(button)
+        bot.send_message(message.chat.id, "Выберете мастера из списка:", reply_markup = keyboard)
+    for hair in total_graf:
+        for master in hair:
+            for j in master:
+                if int(call.data) == j:
+                    bot.send_message(message.chat.id, 'Расписание мастера с учетом замен на текущий месяц:')
+                    bot.send_message(message.chat.id, text = master[2])
+
+bot.infinity_polling()
---
+++ parser_google_sheet.py
@@ -0,0 +1,47 @@
+import gspread
+import pprint
+import time
+import pickle
+import shelve
+from oauth2client.service_account import ServiceAccountCredentials
+
+link = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']   # задаем ссылку на Гугл таблици
+my_creds = ServiceAccountCredentials.from_json_keyfile_name('auth_data.json', link) # формируем данные для входа из json файла
+client = gspread.authorize(my_creds)    # запускаем клиент для связи с таблицами
+sheet = client.open('graf').sheet1    # открываем нужную на таблицу и лист
+day_number = sheet.get_values('A3:AF3')[0]    #считываем номера дней
+day_name = sheet.get_values('A4:AF4')[0]    #считываем названия дней
+hair1 = sheet.get_values('A5:AF12')        #парикмахерская 1
+hair2 = sheet.get_values('A18:AF24')       #парикмахерская 2
+hair3 = sheet.get_values('A28:AF33')       #парикмахерская 3
+hair4 = sheet.get_values('A38:AF41')       #парикмахерская 4
+hair5 = sheet.get_values('A47:AF50')       #парикмахерская 5
+count = 5
+
+def graf_hair(hair):                        #Функция возвращает список из списков графиков работы мастеров парикмахерской
+    global count
+    graf = []
+    for i in hair:                          # i - Строка с имененем мастера и местом его работы или не работы по дням
+        graf_m = []                         # Формируем список с именем и графиком работы мастера
+        graf_m.append(i[0])                 # Добавляем имя мастера
+        l = ''                              #Формируем строку с местом выхода на работу, числом, днем недели
+        for j in range(1, len(i)):
+            if i[j] != '':                  # Проверяем есть ли запись о выходе на работу в ячейке
+                l += i[j] + ' ' + day_name[j] + ' ' + day_number[j] + '\n'      # Добавляем запись о работе, дне, числе
+        count += 1
+        graf_m.append(count)
+        graf_m.append(l)
+        graf.append(graf_m)
+    return graf
+
+total_graf = [graf_hair(hair1),graf_hair(hair2),graf_hair(hair3),graf_hair(hair4),graf_hair(hair5)]  #Формируем общий список с графиками работы всех парикмахерских
+print(total_graf)
+
+while True:                                                   #Записываем список в файл
+    with open("grafic_dump.dat", "wb") as f:
+        pickle.dump(total_graf,f)
+        time.sleep(36000)
+with open("grafic_dump.dat", "rb") as f:
+    dic2 = pickle.load(f)
+print(dic2)
+
